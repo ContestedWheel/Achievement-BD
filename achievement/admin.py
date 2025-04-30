cdcc7d4c49c4b46041c6e126cbd4e9ad713348b4
Mitoooooooopo <106137571+Mitoooooooopo@users.noreply.github.com>
2025-04-30 14:22:12 +0530

    Add files via upload

---
+++ achievement/admin.py
@@ -0,0 +1,27 @@
+from typing import TYPE_CHECKING, Any
+
+from django.contrib import admin
+from django.utils.safestring import mark_safe
+from .models import Achievement, PlayerAchievement
+
+@admin.register(Achievement)
+class AchievementAdmin(admin.ModelAdmin):
+    list_display = ("name", "description", "achievement_emoji_id", "enable")
+    list_editable = ("enable",)
+    search_fields = ("name",)
+    autocomplete_fields  = ("required_balls",)
+    # alternatively, for large Ball sets you can use autocomplete:
+    # autocomplete_fields = ("required_balls",)
+    @admin.display(description="Emojis")
+    def achievement_emoji(self, obj: Achievement):
+        return mark_safe(
+            f'<img src="https://cdn.discordapp.com/emojis/{obj.achievement_emoji_id}.png?size=40" ' >
+            f'title="ID: {obj.achievement_emoji_id}" />'
+        )
+@admin.register(PlayerAchievement)
+class PlayerAchievementAdmin(admin.ModelAdmin):
+    list_display = ("player", "achievement", "unlocked_at")
+    list_filter = ("unlocked_at",)
+    search_fields = ("player__discord_id", "achievement__name")
+
+
---
+++ achievement/models.py
@@ -0,0 +1,58 @@
+from __future__ import annotations
+
+from datetime import timedelta
+from typing import Any, Iterable, cast
+
+from django.contrib import admin
+from django.core.cache import cache
+from django.db import models
+from django.utils.safestring import SafeText, mark_safe
+from django.utils.timezone import now
+from bd_models.models import Ball, Player 
+from ballsdex.settings import settings 
+
+class Achievement(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    achievement_emoji_id = models.BigIntegerField(
+        null=True,
+        blank=True,
+        help_text="Discord emoji ID for achievement"
+    )
+    description = models.TextField()
+    required_balls = models.ManyToManyField(
+        Ball,
+        related_name="achievements",
+        help_text="Which countryballs you need to collect"
+    )
+    enable = models.BooleanField(
+            help_text="on or off the achievements", default=True
+    )
+
+    class Meta:
+        db_table = "achievements"
+
+    def __str__(self):
+        return self.name
+
+class PlayerAchievement(models.Model):
+    player = models.ForeignKey(
+        Player,
+        on_delete=models.CASCADE,
+        related_name="player_achievements"
+    )
+    achievement = models.ForeignKey(
+        Achievement,
+        on_delete=models.CASCADE,
+        related_name="player_achievements"
+    )
+    unlocked_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        db_table = "player_achievement"
+        unique_together = ("player", "achievement")
+        verbose_name = "Player Achievement"
+        verbose_name_plural = "Player Achievements"
+
+    def __str__(self):
+        return f"{self.player} → {self.achievement}" 
+        
