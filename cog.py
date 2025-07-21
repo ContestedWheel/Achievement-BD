6c8116193cc3d4749530c53eff9c7034f90e09a6
noobdog667 <106137571+Mitoooooooopo@users.noreply.github.com>
2025-07-21 18:55:04 +0530

    Update cog.py

--- cog.py
+++ cog.py
@@ -3,11 +3,12 @@ from discord.ext import commands
 from discord import app_commands
 from typing import TYPE_CHECKING
 import discord
- 
+import random
+
 from .models import AchievementRequiredBall, AchievementRequiredSpecial
 from .models import PlayerAchievement
 from .models import Achievement as AchievementModel
-from ballsdex.settings import settings 
+from ballsdex.settings import settings
 from .transformers import AchievementTransform, AchievementEnabledTransform
 from ballsdex.core.utils.paginator import FieldPageSource, Pages
 from ballsdex.core.bot import BallsDexBot
@@ -25,7 +26,7 @@ from ballsdex.core.models import (
     Trade,
     TradeObject,
     balls,
-    specials, 
+    specials,
     Special
 )
 
@@ -35,62 +36,65 @@ class Achievement(commands.GroupCog):
     """
 
     def __init__(self, bot: "BallsDexBot"):
-        self.bot = bot    
-     
+        self.bot = bot
+
     @app_commands.command()
     async def list(self, interaction: discord.Interaction):
         """
         List all available achievements
-        """ 
-        await interaction.response.defer(ephemeral=True) 
-     
+        """
+        await interaction.response.defer(ephemeral=True)
+
         achievement = await AchievementModel.filter(enable=True).all()
-                
         player, _ = await Player.get_or_create(discord_id=interaction.user.id)
 
         claimed_achievements = {
-            pa.achievement_id 
+            pa.achievement_id
             for pa in await PlayerAchievement.filter(player=player)
         }
-                          
+
         if not achievement:
             await interaction.followup.send(
-                "There are no achievements currently registered in the admin panel.", 
+                "There are no achievements currently registered in the admin panel.",
                 ephemeral=True
             )
             return
 
         entries = []
-            
+
         for achievement in achievement:
             name = f"{achievement.name}"
-            description = f"{achievement.description}" 
-            emote = self.bot.get_emoji(achievement.achievement_emoji_id) or ""
-            
+            description = f"{achievement.description}"
+            emote = self.bot.get_emoji(achievement.achievement_emoji_id)
+
             if achievement.id in claimed_achievements:
                 status = "✅"
             else:
-                status = "❌" 
-            
-            entry = (f"{emote} {name}", f"Requirements: {description} {status}")
+                status = "❌"
+
+            entry_lines = [f"Requirements: {description} {status}"]
+
+            rewards = await achievement.reward.all()
+            if rewards:
+                reward_names = [ball.country for ball in rewards]
+                entry_lines.append(f"Rewards: {', '.join(reward_names)}")
+
+            entry = (f"{emote} {name}", "\n".join(entry_lines))
             entries.append(entry)
-            # Modify this to change number shown in box
-        per_page = 5
-            
+
+        per_page = 10
+
         source = FieldPageSource(entries, per_page=per_page, inline=False, clear_description=False)
-        source.embed.description = (
-            f"__**{settings.bot_name} achievementlist**__" 
-        )
+        source.embed.description = f"__**{settings.bot_name} achievementlist**__"
         source.embed.colour = discord.Colour.blurple()
         source.embed.set_author(
-            name=interaction.user.display_name, 
+            name=interaction.user.display_name,
             icon_url=interaction.user.display_avatar.url
         )
-            
+
         pages = Pages(source=source, interaction=interaction, compact=True)
         await pages.start()
-    
-        
+
     @app_commands.command()
     async def claim(
         self,
@@ -98,72 +102,95 @@ class Achievement(commands.GroupCog):
         achievement: AchievementEnabledTransform
     ):
         """
-        claim an achievement.
-        
+        Claim an achievement.
+
         Parameters
         ----------
         achievement: AchievementEnabledTransform
-            The the achievment you want to claim
-        """ 
-        await interaction.response.defer(ephemeral=True) 
+            The achievement you want to claim.
+        """
+        await interaction.response.defer(ephemeral=True)
+        player, _ = await Player.get_or_create(discord_id=interaction.user.id) 
      
-        player, _ = await Player.get_or_create(discord_id=interaction.user.id)  
-
         if await PlayerAchievement.filter(player=player, achievement=achievement).exists():
-            await interaction.followup.send(
-                f"You Already claimed the achievement **{achievement.name}**!", ephemeral=True)        
+            await interaction.followup.send( 
+                f"You Already claimed the achievement **{achievement.name}**!",)
             return
-        
-        player_owned_ball_type_ids = await BallInstance.filter(player=player).values_list("ball__id", flat=True)  
 
-        player_owned_ball_type_ids = list(player_owned_ball_type_ids)
-        
         required_balls = await AchievementRequiredBall.filter(achievement_id=achievement.id).values_list('ball_id', flat=True)
+        required_specials = await AchievementRequiredSpecial.filter(achievement_id=achievement.id).values_list("special_id", flat=True)
 
-        missing_ids = [
-            ball_id
-            for ball_id in required_balls
-            if ball_id not in player_owned_ball_type_ids  
-         ]  
-                  
-        if missing_ids:
-            missing_balls = await Ball.filter(id__in=missing_ids).all()
-            missing_countries = [Ball.country for Ball in missing_balls]
-            await interaction.followup.send(
-                 f"❌ Missing requirements: {', '.join(missing_countries)}", ephemeral=True)
-            return
-    
-        player_ball_instances = await BallInstance.filter(player=player).prefetch_related("ball", "special")
-    
-        special_requirements = await AchievementRequiredSpecial.filter(
-            achievement_id=achievement.id
-        ).prefetch_related('special')
-        
         required_pairs = list(zip(
-           await AchievementRequiredBall.filter(achievement_id=achievement.id).values_list("ball_id", flat=True),
-           await AchievementRequiredSpecial.filter(achievement_id=achievement.id).values_list("special_id", flat=True)
-       ))     
-       
-        for ball_id, special_id in required_pairs:    
+            await AchievementRequiredBall.filter(achievement_id=achievement.id).values_list("ball_id", flat=True),
+            await AchievementRequiredSpecial.filter(achievement_id=achievement.id).values_list("special_id", flat=True)
+        ))
+
+        player_qs = BallInstance.filter(player=player)
+        if achievement.self_catch:
+            player_qs = player_qs.filter(trade_player_id__isnull=True)
+        player_instances = await player_qs.prefetch_related("ball", "special")
+
+        player_owned_ball_ids = {bi.ball_id for bi in player_instances}
+        missing_balls = [
+            await Ball.get(id=ball_id)
+            for ball_id in required_balls
+            if ball_id not in player_owned_ball_ids
+        ]
+
+        if missing_balls:
+            note = "(must be self-catched)" if achievement.self_catch else ""
+            countries = ", ".join(ball.country for ball in missing_balls)
+            await interaction.followup.send(
+                f"❌ Missing required balls {note}: {countries}",
+                ephemeral=True
+            )
+            return
+
+        for ball_id, special_id in required_pairs:
             if special_id is None:
                 continue
-                
-            has_special_ball = False
-            for instance in player_ball_instances:
-                if instance.ball_id == ball_id and instance.special_id == special_id:
-                    has_special_ball = True
-                    break
-        
-            if not has_special_ball:
-                required_ball = await Ball.get(id=ball_id)
-                required_special = await Special.get(id=special_id) 
+            if not any(
+                bi.ball_id == ball_id and bi.special_id == special_id
+                for bi in player_instances
+            ):
+                ball = await Ball.get(id=ball_id)
+                special = await Special.get(id=special_id)
                 await interaction.followup.send(
-                    f"❌ Missing requirements: {required_ball.country} with {required_special}",
+                    f"❌ Missing special countryball: {ball.country} with {special}",
                     ephemeral=True
                 )
-                return 
-             
+                return
+
+        if not required_balls and len(required_specials) > 0:
+            qs = player_qs.filter(special_id__in=required_specials)
+            if achievement.self_catch:
+                qs = qs.filter(trade_player_id__isnull=True)
+
+            count = await qs.count()
+
+            if count < achievement.required_quantity:
+                specials = await Special.filter(id__in=required_specials).all()
+                special_names = ", ".join(str(special) for special in specials)
+                note = " (must be self-catched)" if achievement.self_catch else ""
+
+                await interaction.followup.send(
+                    f"❌ You need {achievement.required_quantity} of these specials{note}: {special_names}, but you have {count}.",
+                    ephemeral=True
+                )
+                return
+
+        rewards = await achievement.reward.all()
+        for reward_ball in rewards:
+            await BallInstance.create(
+                player=player,
+                ball=reward_ball,
+                special=None,
+                health_bonus=random.randint(-settings.max_attack_bonus, settings.max_attack_bonus),
+                attack_bonus=random.randint(-settings.max_attack_bonus, settings.max_attack_bonus),
+            )
+
         await PlayerAchievement.create(player=player, achievement=achievement)
         await interaction.followup.send(
-            f"🎉Congrats, you claimed **{achievement.name}**!", ephemeral=True) 
-            
+            f"🎉 Congrats, you claimed **{achievement.name}**!",
+            ephemeral=True
+        )
